ea3f4813279c42fdfe5f22bffaa5a0f2771dd03b
Yash Chachad <chachadyash786@gmail.com>
2020-10-29 13:42:42 +0530

    Completed editor syncing using pushers!!!!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { React } from "react";
 import { Switch, Route } from "react-router-dom";
 import CodeEditor from "./components/codeEditor";
+import Pusher from "./pages/index";
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/code" component={CodeEditor} />
+        <Route path="/" component={Pusher} />
       </Switch>
-      Hello
     </div>
   );
 }
