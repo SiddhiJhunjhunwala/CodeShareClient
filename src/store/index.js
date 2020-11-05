641a628f820826e9da00c3deb255feb4b4ac5e9a
Yash Chachad <chachadyash786@gmail.com>
2020-11-05 16:10:58 +0530

    Added logger for redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import combinedReducer from "./reducers/index";
-import logger from "redux-logger";
 import thunk from "redux-thunk";
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+  return result;
+};
 
 const preloaded_state = {};
 export default createStore(
